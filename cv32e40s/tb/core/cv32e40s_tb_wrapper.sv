70aae18c821300673d17f8f76e470bb5b76390ce
leemango1998 <319048894@qq.com>
2023-04-10 05:19:53 -0700

    Add support for E40S core testbench using  vsim

--- cv32e40s/tb/core/cv32e40s_tb_wrapper.sv
+++ cv32e40s/tb/core/cv32e40s_tb_wrapper.sv
@@ -40,6 +40,10 @@ module cv32e40s_tb_wrapper
     logic [31:0]                  instr_addr;
     logic [INSTR_RDATA_WIDTH-1:0] instr_rdata;
 
+    logic                         instr_gntpar;
+    logic                         instr_rvalidpar;
+    logic [4:0]                   instr_rchk;
+
     logic                         data_req;
     logic                         data_gnt;
     logic                         data_rvalid;
@@ -48,6 +52,10 @@ module cv32e40s_tb_wrapper
     logic [3:0]                   data_be;
     logic [31:0]                  data_rdata;
     logic [31:0]                  data_wdata;
+
+    logic                         data_gntpar;
+    logic                         data_rvalidpar;
+    logic [4:0]                   data_rchk;
 
     // signals to debug unit
     logic                         debug_req;
@@ -63,21 +71,38 @@ module cv32e40s_tb_wrapper
     // interrupts (only timer for now)
     assign irq_sec     = '0;
 
-//    // core log reports parameter usage and illegal instructions to the logfile
-//    // MIKET: commenting out as the cv32e40s RTL wrapper does this as well.
-//    cv32e40s_core_log
-//     #(
-//          .PULP_XPULP            ( PULP_XPULP            ),
-//          .PULP_CLUSTER          ( PULP_CLUSTER          ),
-//          .FPU                   ( FPU                   ),
-//          .PULP_ZFINX            ( PULP_ZFINX            ))
-//    core_log_i(
-//          .clk_i              ( cv32e40s_core_i.id_stage_i.clk              ),
-//          .is_decoding_i      ( cv32e40s_core_i.id_stage_i.is_decoding_o    ),
-//          .illegal_insn_dec_i ( cv32e40s_core_i.id_stage_i.illegal_insn_dec ),
-//          .mhartid_i          ( cv32e40s_core_i.mhartid_i                   ),
-//          .pc_id_i            ( cv32e40s_core_i.pc_id                       )
-//      );
+    assign instr_gntpar    = ~instr_gnt;
+    assign instr_rvalidpar = ~instr_rvalid;
+    assign instr_rchk      = { ^instr_rdata[ 7: 0],
+                               ^instr_rdata[15: 8],
+                               ^instr_rdata[23:16],
+                               ^instr_rdata[31:24],
+                               ^{1'b0, 1'b0} }; // {instr_err, exokay}
+
+    assign data_gntpar    = ~data_gnt;
+    assign data_rvalidpar = ~data_rvalid;
+    assign data_rchk      = { ^data_rdata[ 7: 0],
+                              ^data_rdata[15: 8],
+                              ^data_rdata[23:16],
+                              ^data_rdata[31:24],
+                              ^{1'b0, 1'b0} }; // {data_err, exokay}
+
+    //    // core log reports parameter usage and illegal instructions to the logfile
+    //    // MIKET: commenting out as the cv32e40s RTL wrapper does this as well.
+    //    cv32e40s_core_log
+    //     #(
+    //          .PULP_XPULP            ( PULP_XPULP            ),
+    //          .PULP_CLUSTER          ( PULP_CLUSTER          ),
+    //          .FPU                   ( FPU                   ),
+    //          .PULP_ZFINX            ( PULP_ZFINX            ),
+    //          .NUM_MHPMCOUNTERS      ( NUM_MHPMCOUNTERS      ))
+    //    core_log_i(
+    //          .clk_i              ( cv32e40s_core_i.id_stage_i.clk              ),
+    //          .is_decoding_i      ( cv32e40s_core_i.id_stage_i.is_decoding_o    ),
+    //          .illegal_insn_dec_i ( cv32e40s_core_i.id_stage_i.illegal_insn_dec ),
+    //          .mhartid_i          ( cv32e40s_core_i.mhartid_i                   ),
+    //          .pc_id_i            ( cv32e40s_core_i.pc_id                       )
+    //      );
 
     // instantiate the core
     cv32e40s_core cv32e40s_core_i
@@ -87,36 +112,87 @@ module cv32e40s_tb_wrapper
          
          .scan_cg_en_i           ( '0                    ),
 
-         .boot_addr_i            ( BOOT_ADDR             ),
+         // Static configuration
+         .boot_addr_i            ( 32'h80                ),
+         .dm_exception_addr_i    ( '0                    ),
          .dm_halt_addr_i         ( DM_HALTADDRESS        ),
          .mhartid_i              ( HART_ID               ),
          .mimpid_patch_i         ( IMP_PATCH_ID          ),
+         .mtvec_addr_i           ( '0                    ), 
 
-         .instr_req_o            ( instr_req             ),
-         .instr_gnt_i            ( instr_gnt             ),
-         .instr_rvalid_i         ( instr_rvalid          ),
-         .instr_addr_o           ( instr_addr            ),
+         // Instruction memory interface
+         .instr_req_o            ( instr_req             ),        
+         .instr_gnt_i            ( instr_gnt             ),      
+         .instr_rvalid_i         ( instr_rvalid          ),     
+         .instr_addr_o           ( instr_addr            ),    
+         .instr_memtype_o        (                       ),
+         .instr_prot_o           (                       ),
+         .instr_dbg_o            (                       ),
          .instr_rdata_i          ( instr_rdata           ),
+         .instr_err_i            ( 1'b0                  ),
 
-         .data_req_o             ( data_req              ),
-         .data_gnt_i             ( data_gnt              ),
-         .data_rvalid_i          ( data_rvalid           ),
+         .instr_reqpar_o         (                       ),
+         .instr_gntpar_i         ( instr_gntpar          ),
+         .instr_rvalidpar_i      ( instr_rvalidpar       ),
+         .instr_achk_o           (                       ),
+         .instr_rchk_i           ( instr_rchk            ),
+         
+         // Data memory interface
+         .data_req_o             ( data_req              ),        
+         .data_gnt_i             ( data_gnt              ),   
+         .data_rvalid_i          ( data_rvalid           ),    
          .data_we_o              ( data_we               ),
          .data_be_o              ( data_be               ),
-         .data_addr_o            ( data_addr             ),
+         .data_addr_o            ( data_addr             ),    
+         .data_memtype_o         (                       ),
+         .data_prot_o            (                       ),
+         .data_dbg_o             (                       ),
          .data_wdata_o           ( data_wdata            ),
          .data_rdata_i           ( data_rdata            ),
+         .data_err_i             ( 1'b0                  ),
+
+         .data_reqpar_o          (                       ),
+         .data_gntpar_i          ( data_gntpar           ),
+         .data_rvalidpar_i       ( data_rvalidpar        ),
+         .data_achk_o            (                       ),
+         .data_rchk_i            ( data_rchk             ),
+
+         // Cycle Count
+         .mcycle_o               (                       ),
 
          // Interrupts verified in UVM environment
          .irq_i                  ( {32{1'b0}}            ),
-         .irq_ack_o              ( irq_ack               ),
-         .irq_id_o               ( irq_id_out            ),
 
-         .debug_req_i            ( debug_req             ),
+           // Event wakeup signals
+         .wu_wfe_i               ( 1'b0                  ),
 
+         // CLIC Interface
+         .clic_irq_i             ( '0                    ),
+         .clic_irq_id_i          ( '0                    ),
+         .clic_irq_level_i       ( '0                    ),
+         .clic_irq_priv_i        ( '0                    ),
+         .clic_irq_shv_i         ( '0                    ),
+
+         // Fencei flush handshake
+         .fencei_flush_req_o     (                       ),
+         .fencei_flush_ack_i     ( 1'b0                  ),
+
+         // Security Alerts
+         .alert_major_o          (                       ),
+         .alert_minor_o          (                       ),
+
+         // Debug interface
+         .debug_req_i            ( 1'b0                  ),
+         .debug_havereset_o      (                       ),
+         .debug_running_o        (                       ),
+         .debug_halted_o         (                       ),
+         .debug_pc_valid_o       (                       ),
+         .debug_pc_o             (                       ),
+
+         // CPU Control Signals
          .fetch_enable_i         ( fetch_enable_i        ),
-         .core_sleep_o           ( core_sleep_o          )
-       );
+         .core_sleep_o           (                       )
+      );
 
     // this handles read to RAM and memory mapped pseudo peripherals
     mm_ram
